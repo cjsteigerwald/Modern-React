ea4c671a23d01a4ab562d36c546612c0dc5df677
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-08-17 04:56:45 -0700

    finishing dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,27 +1,53 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+import React, { useState } from "react";
+// import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
+// import Search from "./components/Search";
 
-const items = [
+// const items = [
+// 	{
+// 		title: "What is React?",
+// 		content: "React is a fron end javascript framework",
+// 	},
+// 	{
+// 		title: "Why use React?",
+// 		content: "React is a favorite JS Library among engineers",
+// 	},
+// 	{
+// 		title: "How do you use React?",
+// 		content: "You use React by creating components",
+// 	},
+// ];
+
+const options = [
 	{
-		title: "What is React?",
-		content: "React is a fron end javascript framework",
+		label: "The Color Red",
+		value: "red",
 	},
 	{
-		title: "Why use React?",
-		content: "React is a favorite JS Library among engineers",
+		label: "The Color Green",
+		value: "green",
 	},
 	{
-		title: "How do you use React?",
-		content: "You use React by creating components",
+		label: "A Shade of Blue",
+		value: "blue",
 	},
 ];
 
 export default () => {
+	const [showDropdown, setDropdown] = useState(true);
+	const [selected, setSelected] = useState(options[0]);
 	return (
 		<div>
-			{/* <Accordion items={items} /> */}
-			<Search />
+			<button onClick={() => setDropdown(!showDropdown)}>
+				Toggle Dropdown
+			</button>
+			{showDropdown ? (
+				<Dropdown
+					options={options}
+					selected={selected}
+					onSelectedChange={setSelected}
+				/>
+			) : null}
 		</div>
 	);
 };
